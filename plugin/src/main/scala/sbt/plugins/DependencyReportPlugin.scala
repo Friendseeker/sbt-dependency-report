68e8667be1f8c1a26529767e48380ebc329b029d
friendseeker <66892505+Friendseeker@users.noreply.github.com>
2024-10-29 22:44:06 -0700

    Include config name as part of file name

--- plugin/src/main/scala/sbt/plugins/DependencyReportPlugin.scala
+++ plugin/src/main/scala/sbt/plugins/DependencyReportPlugin.scala
@@ -19,13 +19,13 @@ object DependencyReportPlugin extends AutoPlugin {
   override def trigger = allRequirements
   override def requires = MiniDependencyTreePlugin
 
-  override lazy val projectSettings: Seq[Setting[_]] = Seq(
+  override lazy val projectSettings: Seq[Setting[?]] = Seq(
     // Define the dependencyReport task for each configuration using inConfig
     inConfig(Compile)(baseDependencyReportSettings),
     inConfig(Test)(baseDependencyReportSettings),
   ).flatten
 
-  private lazy val baseDependencyReportSettings: Seq[Setting[_]] = Seq(
+  private lazy val baseDependencyReportSettings: Seq[Setting[?]] = Seq(
     dependencyReport := {
       val defaultFormat = "text"
 
@@ -39,9 +39,48 @@ object DependencyReportPlugin extends AutoPlugin {
 
       val graph = dependencyTreeModuleGraph0.value
 
+      val configName = configuration.value.name
+
       def printAndPersistReport(report: String, fileExtension: String, targetDir: File): File = {
         streams.value.log.info(report)
         persistReport(report, fileExtension, targetDir)
+      }
+
+      def generateTextReport(graph: ModuleGraph, format: String, graphWidth: Int): String = {
+        val subtype = format.stripPrefix("text:")
+        subtype match {
+          case "list" => rendering.FlatList.render(_.id.idString)(graph)
+          case "stats" => rendering.Statistics.renderModuleStatsList(graph)
+          case "info" => rendering.LicenseInfo.render(graph)
+          case _ => rendering.AsciiTree.asciiTree(graph, graphWidth)
+        }
+      }
+
+      def createReportFile(targetDir: File, fileExtension: String): File = {
+        new File(targetDir, s"dependencies-$configName.$fileExtension")
+      }
+
+      def persistReport(report: String, fileExtension: String, targetDir: File): File = {
+        val target = createReportFile(targetDir, fileExtension)
+        IO.write(target, report, IO.utf8)
+        target
+      }
+
+      def generateJsonReport(graph: ModuleGraph): String = {
+        TreeView.createJson(graph)
+      }
+
+      def generateAndPersistHtmlReport(graph: ModuleGraph, targetDir: File): File = {
+        val renderedTree = TreeView.createJson(graph)
+        val target = createReportFile(targetDir, "html")
+        TreeView.createLink(renderedTree, targetDir)
+        target
+      }
+
+      def generateAndPersistGraphMLReport(graph: ModuleGraph, targetDir: File): File = {
+        val target = createReportFile(targetDir, "xml")
+        rendering.GraphML.saveAsGraphML(graph, target.getAbsolutePath)
+        target
       }
 
       val serializedReport = format match {
@@ -59,41 +98,4 @@ object DependencyReportPlugin extends AutoPlugin {
       streams.value.log.info(s"Dependency report written to ${serializedReport.getAbsolutePath}")
     }
   )
-
-  private def generateTextReport(graph: ModuleGraph, format: String, graphWidth: Int): String = {
-    val subtype = format.stripPrefix("text:")
-    subtype match {
-      case "list" => rendering.FlatList.render(_.id.idString)(graph)
-      case "stats" => rendering.Statistics.renderModuleStatsList(graph)
-      case "info" => rendering.LicenseInfo.render(graph)
-      case _ => rendering.AsciiTree.asciiTree(graph, graphWidth)
-    }
-  }
-
-  private def createReportFile(targetDir: File, fileExtension: String): File = {
-    new File(targetDir, s"dependencies.$fileExtension")
-  }
-
-  private def persistReport(report: String, fileExtension: String, targetDir: File): File = {
-    val target = createReportFile(targetDir, fileExtension)
-    IO.write(target, report, IO.utf8)
-    target
-  }
-
-  private def generateJsonReport(graph: ModuleGraph): String = {
-    TreeView.createJson(graph)
-  }
-
-  private def generateAndPersistHtmlReport(graph: ModuleGraph, targetDir: File): File = {
-    val renderedTree = TreeView.createJson(graph)
-    val target = createReportFile(targetDir, "html")
-    TreeView.createLink(renderedTree, target)
-    target
-  }
-
-  private def generateAndPersistGraphMLReport(graph: ModuleGraph, targetDir: File): File = {
-    val target = createReportFile(targetDir, "xml")
-    rendering.GraphML.saveAsGraphML(graph, target.getAbsolutePath)
-    target
-  }
 }
